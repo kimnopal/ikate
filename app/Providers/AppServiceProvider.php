3fe53931cc9a43d02330b4e5019b5cf322b58ff2
kimnopal <naufalhakim366@gmail.com>
2025-07-14 02:52:14 +0700

    fix(last): ngantuk bang

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,10 +18,5 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        if (env('APP_ENV') == 'production') {
-            URL::forceScheme('https');
-        }
-    }
+    public function boot(): void {}
 }
--- config/inertia.php
+++ config/inertia.php
@@ -17,7 +17,7 @@ return [
 
     'ssr' => [
         'enabled' => true,
-        'url' => 'http://127.0.0.1:13714',
+        'url' => env('APP_ENV') === 'production' ? 'https://ikate.vercel.app' : 'http://127.0.0.1:13714',
         // 'bundle' => base_path('bootstrap/ssr/ssr.mjs'),
 
     ],
