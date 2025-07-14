64bfbb9914cd4901154d90bed1a1d59711cb02bc
kimnopal <naufalhakim366@gmail.com>
2025-07-14 16:07:36 +0700

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
         },
     },
     build: {
-        outDir: 'build',
+        outDir: 'public/build',
         rollupOptions: {
             output: {
                 manualChunks: undefined,
